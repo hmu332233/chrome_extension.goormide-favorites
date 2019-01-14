5fb14cded80612ebaf1fcb5e5b543893bcd937d6
minung.han <hmu332233@gmail.com>
2019-01-15 00:10:28 +0900

    [Modify] 즐겨찾기 탭 추가

--- pageScript.js
+++ pageScript.js
@@ -1,1 +1,23 @@
 // pageScript.js
+
+function makeTabElement () {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.setAttribute('id', 'gLayoutTab_command');
+  a.setAttribute('href', '#favorites_explorer_tab');
+  const span = document.createElement('span');
+  span.textContent = '즐겨찾기';
+
+  a.appendChild(span);
+  li.appendChild(a);
+
+  return li;
+}
+
+function injectTab(tab) {
+  const westTabDom = document.querySelector('#west_tab');
+  westTabDom.appendChild(tab);
+}
+
+const newTab = makeTabElement();
+injectTab(newTab);
