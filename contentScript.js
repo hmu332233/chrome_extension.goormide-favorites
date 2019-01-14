a5427772f47282e9e5cf29ba37d8d8df92078de5
minung.han <hmu332233@gmail.com>
2019-01-14 23:51:18 +0900

    [Modify] pageScript 웹페이지에 주입되도록

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,8 @@
 // contentScript.js
+
+// inject pageScript
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('pageScript.js');
+(document.head || document.documentElement).appendChild(s);
+
+s.parentNode.removeChild(s);
